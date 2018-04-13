d8f3926fcc145e4995a8220ce1b14a29f326ab3f
Unknown <sixinhong1993@163.com>
2018-04-13 10:00:14 +0800

    倒计时模块

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -9639,24 +9639,59 @@ __webpack_require__(334);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-// class Timer{
-//     countdown(end,update,handle){
-//         const now=new Date().getTime();
-//         const self=this;
-//         if(now-end){
-//             handle.call(self);
-//         }else{
-//             let last_time=end-now;
-//             const px_d=1000*60*60*24;//常量一天等于多少毫秒
-//             const px_h=1000*60*60;
-//             const px_m=1000*60;
-//             const px_s=1000;
-//
-//
-//         }
-//     }
-// }
 
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Timer = function () {
+    function Timer() {
+        _classCallCheck(this, Timer);
+    }
+
+    _createClass(Timer, [{
+        key: 'countdown',
+        value: function countdown(end, update, handle) {
+            var now = new Date().getTime();
+            var self = this;
+            if (now - end) {
+                handle.call(self);
+            } else {
+                var last_time = end - now;
+                var px_d = 1000 * 60 * 60 * 24; //常量一天等于多少毫秒
+                var px_h = 1000 * 60 * 60;
+                var px_m = 1000 * 60;
+                var px_s = 1000;
+                var d = Math.floor(last_time / px_d);
+                var h = Math.floor((last_time - d * px_d) / px_h);
+                var m = Math.floor((last_time - d * px_d - h * px_h) / px_m);
+                var s = Math.floor((last_time - d * px_d - h * px_h - m * px_m) / px_s);
+
+                var r = [];
+                if (d) {
+                    r.push('<em>' + d + '</em>\u5929');
+                }
+                if (r.length || h > 0) {
+                    r.push('<em>' + h + '</em>\u65F6');
+                }
+                if (r.length || m > 0) {
+                    r.push('<em>' + m + '</em>\u5206');
+                }
+                if (r.length || s > 0) {
+                    r.push('<em>' + s + '</em>\u79D2');
+                }
+                self.last_time = r.join('');
+                update.call(self, r, join(''));
+                setTimeout(function () {
+                    self.countdown(end, update, handle);
+                }, 1000);
+            }
+        }
+    }]);
+
+    return Timer;
+}();
 
 /***/ })
 /******/ ]);
